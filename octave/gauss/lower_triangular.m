a378f424f5f703b526dc2fa4de4d9ebd3475d269
hexdhog <hexdhog@gmail.com>
2022-09-25 14:39:16 +0200

    fixed lower triangular matrix solver & added functions to main.m

--- octave/gauss/lower_triangular.m
+++ octave/gauss/lower_triangular.m
@@ -3,14 +3,15 @@ function [result] = lower_triangular(matrix, ind)
 	mrows = rows(matrix);
 	mcols = columns(matrix);
 	result = zeros(mcols, 1);
-	result(mcols) = ind(1) / matrix(1, mcols);
+	result(1) = ind(1) / matrix(1, 1);
 
 	for i = 2:mrows
 		ind_result = ind(i);
-		for j = mcols - (i - 2):mcols
+		for j = i - 1:-1:1
+			prev = ind_result;
 			ind_result -= matrix(i, j) * result(j);
 		end
-		result(mrows - (i - 1)) = ind_result / matrix(i, mcols - (i - 1));
+		result(i) = ind_result / matrix(i, i);
 	end
 end
 
--- octave/gauss/main.m
+++ octave/gauss/main.m
@@ -1,73 +1,110 @@
-# printf("Upper Triangular\r\n")
-# matrix = [1, 2, 3; 0, 1, -1; 0, 0, 1];
-# ind = [3, -1, 2];
-# result = upper_triangular(matrix, ind);
-# disp(result);
-#
-# printf("Lower Triangular\r\n")
-# matrix = [0, 0, 1; 0, 1, -1; 1, 2, 3];
-# ind = [2, -1, 3];
-# result = lower_triangular(matrix, ind);
-# disp(result);
+function main
+	test_upper_triangular()
+	test_lower_triangular()
+end
 
-printf("Gaussify\r\n");
-printf("Start matrix:\r\n");
-matrix = [1, 3, 0;  2, 1, -1; 2, 4, -1];
-ind = [4, 0, 2];
-disp(matrix);
-printf("Ind terms:\r\n");
-disp(ind);
-printf("\r\n");
+function test_upper_triangular
+	printf("Upper Triangular\r\n")
+	printf("Start matrix:\r\n");
+	matrix = [
+		1, 2, 3;
+		0, 1, -1;
+		0, 0, 1
+	];
+	ind = [3, -1, 2];
+	disp(matrix);
+	printf("Ind terms:\r\n");
+	disp(ind);
+	printf("\r\n");
 
-printf("Processed matrix:\r\n");
-[rmat, rind] = gaussify(matrix, ind);
-disp(rmat);
-printf("Ind terms:\r\n");
-disp(rind);
-printf("\r\n");
+	printf("Solution:\r\n");
+	result = upper_triangular(matrix, ind);
+	disp(result);
+end
 
-printf("Solution:\r\n");
-result = upper_triangular(rmat, rind);
-disp(result);
+function test_lower_triangular
+	printf("Lower Triangular\r\n")
+	printf("Start matrix:\r\n");
+	matrix = [
+		4, 0, 0;
+		3, -9, 0;
+		-8, 1, 2
+	];
+	ind = [-1, 6, 8];
+	disp(matrix);
+	printf("Ind terms:\r\n");
+	disp(ind);
+	printf("\r\n");
 
-# printf("Gaussify Pivot\r\n");
-# printf("Start matrix:\r\n");
-# matrix = [1, 3, 0;  2, 1, -1; 2, 4, -1];
-# ind = [4, 0, 2];
-# disp(matrix);
-# printf("Ind terms:\r\n");
-# disp(ind);
-# printf("\r\n");
-#
-# printf("Processed matrix:\r\n");
-# [rmat, rind] = gaussify_pivot(matrix, ind);
-# disp(rmat);
-# printf("Ind terms:\r\n");
-# disp(rind);
-# printf("\r\n");
-#
-# printf("Solution:\r\n");
-# result = upper_triangular(rmat, rind);
-# disp(result);
+	printf("Solution:\r\n");
+	result = lower_triangular(matrix, ind);
+	disp(result);
+end
 
-printf("Gaussify Pivot Maximal\r\n");
-printf("Start matrix:\r\n");
-matrix = [1, 3, 0;  2, 1, -1; 2, 4, -1];
-ind = [4, 0, 2];
-disp(matrix);
-printf("Ind terms:\r\n");
-disp(ind);
-printf("\r\n");
+function test_gaussify
+	printf("Gaussify\r\n");
+	printf("Start matrix:\r\n");
+	matrix = [1, 3, 0;  2, 1, -1; 2, 4, -1];
+	ind = [4, 0, 2];
+	disp(matrix);
+	printf("Ind terms:\r\n");
+	disp(ind);
+	printf("\r\n");
+	printf("Processed matrix:\r\n");
+	[rmat, rind] = gaussify(matrix, ind);
+	disp(rmat);
+	printf("Ind terms:\r\n");
+	disp(rind);
+	printf("\r\n");
 
-printf("Processed matrix:\r\n");
-[rmat, rind, pos] = gaussify_pivot_max(matrix, ind);
-disp(rmat);
-printf("Ind terms:\r\n");
-disp(rind);
-printf("Position:\r\n");
-disp(pos);
-printf("\r\n");
+	printf("Solution:\r\n");
+	result = upper_triangular(rmat, rind);
+	disp(result);
+end
 
-printf("Solution:\r\n");
-result = upper_triangular(rmat, rind);
-disp(result);
+function test_gauss_partial_pivot
+	printf("Gaussify Pivot\r\n");
+	printf("Start matrix:\r\n");
+	matrix = [1, 3, 0;  2, 1, -1; 2, 4, -1];
+	ind = [4, 0, 2];
+	disp(matrix);
+	printf("Ind terms:\r\n");
+	disp(ind);
+	printf("\r\n");
+
+	printf("Processed matrix:\r\n");
+	[rmat, rind] = gaussify_pivot(matrix, ind);
+	disp(rmat);
+	printf("Ind terms:\r\n");
+	disp(rind);
+	printf("\r\n");
+
+	printf("Solution:\r\n");
+	result = upper_triangular(rmat, rind);
+	disp(result);
+end
+
+function test_gauss_maximal_pivot
+	printf("Gaussify Pivot Maximal\r\n");
+	printf("Start matrix:\r\n");
+	matrix = [1, 3, 0;  2, 1, -1; 2, 4, -1];
+	ind = [4, 0, 2];
+	disp(matrix);
+	printf("Ind terms:\r\n");
+	disp(ind);
+	printf("\r\n");
+
+	printf("Processed matrix:\r\n");
+	[rmat, rind, pos] = gaussify_pivot_max(matrix, ind);
+	disp(rmat);
+	printf("Ind terms:\r\n");
+	disp(rind);
+	printf("Position:\r\n");
+	disp(pos);
+	printf("\r\n");
+
+	printf("Solution:\r\n");
+	result = upper_triangular(rmat, rind);
+	disp(result);
+end
+
